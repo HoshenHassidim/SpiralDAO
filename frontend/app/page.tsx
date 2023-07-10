13e45a6715863a6223f59936e7deec3ab6cc2aac
AdamEli <50723103+AdamEli@users.noreply.github.com>
2023-07-10 03:40:53 -0400

    Installed Next.js app

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+// import Navbar from '../components/Navbar'
+export default function Home() {
+  return (
+    <main className="flex h-full justify-center items-center">
+      <button className="bg-gray-500 rounded-md">Connect</button>
+      <button className="bg-gray-500 rounded-md">Register member</button>
+    </main>
+  )
+}
