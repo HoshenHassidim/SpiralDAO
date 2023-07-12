c4033cb31c57b6031fa14f7a36cb21d4b51d9bcc
abstelz <139104068+abstelz@users.noreply.github.com>
2023-07-12 09:44:46 +0300

    Update Problems.sol
    Can now change vote

--- backend/contracts/Problems.sol
+++ backend/contracts/Problems.sol
@@ -13,7 +13,7 @@ contract Problems {
     }
 
     // The Problem struct defines the structure for each problem
-    // Each problem has an id, creator, name, ratingSum, ratingCount, openForRating status, and a mapping of addresses that have rated it
+    // Each problem has an id, creator, name, ratingSum, ratingCount, openForRating status, and a mapping of addresses' old ratings
     struct Problem {
         uint id;
         address creator;
@@ -21,7 +21,7 @@ contract Problems {
         uint ratingSum;
         uint ratingCount;
         bool openForRating;
-        mapping(address => bool) hasRated;
+        mapping(address => uint) oldRating;
     }
 
     // This is a counter for the problems raised, serving as the unique identifier for each problem
@@ -92,11 +92,13 @@ contract Problems {
         Problem storage problem = problems[_problemId];
 
         require(problem.openForRating, "Problem is closed for rating.");
-        require(!problem.hasRated[msg.sender], "You have already rated this problem.");
-
+        if(problem.oldRating[msg.sender] > 0) {
+           problem.ratingSum -= problem.oldRating[msg.sender];
+        } else {
+           problem.ratingCount++;
+        }
+        problem.oldRating[msg.sender] = _rating;
         problem.ratingSum += _rating;
-        problem.ratingCount++;
-        problem.hasRated[msg.sender] = true;
 
         emit ProblemRated(_problemId, msg.sender, _rating);
     }
