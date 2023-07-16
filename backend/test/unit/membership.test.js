a55c9067d8e3c2efa30e55a29ec39aa0dbf90e17
jastroff <bocaj.a31@gmail.com>
2023-07-16 04:56:06 -0400

    Projects.sol updated with Custom Errors, as well as problems.test.js

--- backend/test/unit/membership.test.js
+++ backend/test/unit/membership.test.js
@@ -1,95 +1,96 @@
-const { expect } = require("chai")
-const { ethers } = require("hardhat")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Membership", function () {
-    let Membership, membership, deployer, addr1
-    let deployerAddress, addr1Address
+    let Membership, membership, deployer, addr1;
+    let deployerAddress, addr1Address;
 
     before(async function () {
-        Membership = await ethers.getContractFactory("Membership")
-        ;[deployer, addr1] = await ethers.getSigners()
+        Membership = await ethers.getContractFactory("Membership");
+        [deployer, addr1] = await ethers.getSigners();
 
-        deployerAddress = await deployer.getAddress()
-        addr1Address = await addr1.getAddress()
-    })
+        deployerAddress = await deployer.getAddress();
+        addr1Address = await addr1.getAddress();
+    });
 
     beforeEach(async function () {
-        membership = await Membership.deploy()
-        await membership.deployed()
-    })
+        membership = await Membership.deploy();
+        await membership.deployed();
+    });
 
     describe("registerMember", function () {
         it("Should register a new member successfully", async function () {
-            await membership.connect(deployer).registerMember("deployer")
-            let isMember = await membership.isRegisteredMember(deployerAddress)
-            expect(isMember).to.equal(true)
-        })
+            await membership.connect(deployer).registerMember("deployer");
+            let isMember = await membership.isRegisteredMember(deployerAddress);
+            expect(isMember).to.equal(true);
+        });
 
         it("Should not register a member with an existing username", async function () {
-            await membership.connect(deployer).registerMember("common")
+            await membership.connect(deployer).registerMember("common");
             await expect(membership.connect(addr1).registerMember("common")).to.be.revertedWith(
-                "Username already exists"
-            )
-        })
+                "UsernameAlreadyExists"
+            );
+        });
 
         it("Should not register a member twice", async function () {
-            await membership.connect(deployer).registerMember("deployer")
+            await membership.connect(deployer).registerMember("deployer");
             await expect(
                 membership.connect(deployer).registerMember("deployer")
-            ).to.be.revertedWith("Already a member.")
-        })
-    })
+            ).to.be.revertedWith("AlreadyMember");
+        });
+    });
 
     describe("unregisterMember", function () {
         it("Should unregister a member successfully", async function () {
-            await membership.connect(deployer).registerMember("deployer")
-            await membership.connect(deployer).unregisterMember()
-            let isMember = await membership.isRegisteredMember(deployerAddress)
-            expect(isMember).to.equal(false)
-        })
+            await membership.connect(deployer).registerMember("deployer");
+            await membership.connect(deployer).unregisterMember();
+            let isMember = await membership.isRegisteredMember(deployerAddress);
+            expect(isMember).to.equal(false);
+        });
 
         it("Should not unregister a non-member", async function () {
             await expect(membership.connect(deployer).unregisterMember()).to.be.revertedWith(
-                "Not a member."
-            )
-        })
-    })
+                "NotMember"
+            );
+        });
+    });
 
     describe("isRegisteredMember", function () {
         it("Should return false for non-member", async function () {
-            let isMember = await membership.isRegisteredMember(deployerAddress)
-            expect(isMember).to.equal(false)
-        })
+            let isMember = await membership.isRegisteredMember(deployerAddress);
+            expect(isMember).to.equal(false);
+        });
 
         it("Should return true for member", async function () {
-            await membership.connect(deployer).registerMember("deployer")
-            let isMember = await membership.isRegisteredMember(deployerAddress)
-            expect(isMember).to.equal(true)
-        })
-    })
+            await membership.connect(deployer).registerMember("deployer");
+            let isMember = await membership.isRegisteredMember(deployerAddress);
+            expect(isMember).to.equal(true);
+        });
+    });
 
     describe("isUsernameTaken", function () {
         it("Should return false for non-taken username", async function () {
-            let isTaken = await membership.isUsernameTaken("deployer")
-            expect(isTaken).to.equal(false)
-        })
+            let isTaken = await membership.isUsernameTaken("deployer");
+            expect(isTaken).to.equal(false);
+        });
 
         it("Should return true for taken username", async function () {
-            await membership.connect(deployer).registerMember("deployer")
-            let isTaken = await membership.isUsernameTaken("deployer")
-            expect(isTaken).to.equal(true)
-        })
-    })
+            await membership.connect(deployer).registerMember("deployer");
+            let isTaken = await membership.isUsernameTaken("deployer");
+            expect(isTaken).to.equal(true);
+        });
+    });
 
     describe("getMember", function () {
         it("Should return the username of a member", async function () {
-            await membership.connect(deployer).registerMember("deployer")
-            let username = await membership.getMember(deployerAddress)
-            expect(username).to.equal("deployer")
-        })
+            await membership.connect(deployer).registerMember("deployer");
+            let username = await membership.getMember(deployerAddress);
+            expect(username).to.equal("deployer");
+        });
 
         it("Should revert when attempting to get username of non-member", async function () {
-            await expect(membership.getMember(deployerAddress)).to.be.revertedWith("Not a member.")
-        })
-    })
-})
+            await expect(membership.getMember(deployerAddress)).to.be.revertedWith("NotMember");
+        });
+    });
+});
+
